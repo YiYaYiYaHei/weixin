22bc61c48ffdd5adf5d9716b4e98a9abbed4b9f1
YiYaYiYaHei <1823972245@qq.com>
2022-01-11 18:02:32 +0800

    feature--【uni-app 微信】代码优化

--- ShoppingMall/ShoppingMall/pages/login/start.vue
+++ ShoppingMall/ShoppingMall/pages/login/start.vue
@@ -12,7 +12,7 @@ import user from '../../mixins/user.js';
 export default {
   mixins: [user],
   data() {
-    return {}
+    return {};
   },
   onLoad(params) {
     // 页面加载--版本更新监听
@@ -26,30 +26,24 @@ export default {
         // 调用更新token接口,若有用户权限操作，更新完跳转至对应页面即可
         let loginInfo = JSON.parse(uni.getStorageSync('userInfo') || '{}');
         if (!loginInfo.openId) {
-          uni.reLaunch({
-            url: '/pages/login/login'
-          });
+          uni.reLaunch({url: '/pages/login/login'});
           return;
         }
         let result = await this.$apis.login.refreshToken({openId: loginInfo.openId});
         if (result.status === 200) {
           uni.setStorageSync('token', result.data.token);
           // 若有用户权限操作，存储完跳转至对应页面即可;
-          uni.switchTab({
-            url: '/pages/home/index'
-          });
+          uni.switchTab({url: '/pages/home/index'});
         } else {
           this.$uniTools.showToast({title: result.message});
         }
       } else {
         // reLaunch--关闭所有页面，打开到应用内的某个页面。
-        uni.reLaunch({
-          url: '/pages/login/login'
-        });
+        uni.reLaunch({url: '/pages/login/login'});
       }
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
